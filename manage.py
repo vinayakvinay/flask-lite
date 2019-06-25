f50139b8a774e811df23a48cb6f1e84dcbf070c7
Vinay <Vinay>
2019-06-25 08:25:16 +0530

    py changes

--- manage.py
+++ manage.py
@@ -29,15 +29,6 @@ manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-@manager.command
 def reset_db():
     """
     Recreates a local database. You probably should not use this on
